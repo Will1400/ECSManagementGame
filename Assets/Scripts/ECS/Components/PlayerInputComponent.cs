c442213939263dfa9c0fc92947b08d9db86c749a
Will1400 <will1400@edu.campusvejle.dk>
2020-02-19 14:35:45 +0100

    Files

---
+++ Assets/Scripts/ECS/Components/PlayerInputComponent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct PlayerInputComponent : IComponentData
+{
+    public bool LeftClick;
+    public bool RightClick;
+}
---
+++ Assets/Scripts/ECS/Systems/PlayerInputSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+
+public class PlayerInputSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new PlayerInputJob
+        {
+            leftClick = Input.GetMouseButton(0),
+            rightClick = Input.GetMouseButton(1)
+        };
+
+        return job.Schedule(this, inputDeps);
+    }
+
+    struct PlayerInputJob : IJobForEach<PlayerInputComponent>
+    {
+        public bool leftClick;
+        public bool rightClick;
+
+        public void Execute(ref PlayerInputComponent input)
+        {
+            input.LeftClick = leftClick;
+            input.RightClick = rightClick;
+        }
+    }
+}
