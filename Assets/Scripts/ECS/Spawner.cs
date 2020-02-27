e38f830cbbae536be006da22a245501b6189fb8c
Will1400 <will1400@edu.campusvejle.dk>
2020-02-27 11:49:48 +0100

    Added spawner script for testing

---
+++ Assets/Scripts/ECS/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefabToSpawn;
+    [SerializeField]
+    int amountToSpawn;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            for (int i = 0; i < amountToSpawn; i++)
+            {
+                Instantiate(prefabToSpawn);
+            }
+        }
+    }
+}
