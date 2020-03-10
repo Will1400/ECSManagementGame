084aa990c54c57b940162440418c2011670203fc
Will1400 <will1400@edu.campusvejle.dk>
2020-03-10 11:17:35 +0100

    Added existing astar (Spikes to 60ms)

---
+++ Assets/Scripts/ECS/Systems/Pathfinding/Existing AStar/AStar.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Mathematics;
+
+public class AStar
+{
+    private NativeArray<int> grid;
+    private int2 gridSize;
+
+    [BurstCompile]
+    public NativeList<int2> GetPath(NativeArray<int> grid, int2 gridSize, NativeArray<int2> neighbourOffsets, int2 start, int2 end)
+    {
+        this.gridSize = gridSize;
+        this.grid = grid;
+        NativeList<int2> path = new NativeList<int2>();
+
+        Node startNode = new Node(start);
+        Node endNode = new Node(end);
+
+        Heap<Node> openList = new Heap<Node>(gridSize.x * gridSize.y);
+        HashSet<Node> closedList = new HashSet<Node>();
+
+        openList.Add(startNode);
+        Node currentNode = null;
+
+        while (openList.Count > 0 && !closedList.Select(x => x.Position).Contains(end)) // Loop til end is found
+        {
+            currentNode = openList.RemoveFirst();
+
+            closedList.Add(currentNode);
+
+
+            for (int i = 0; i < neighbourOffsets.Length; i++)
+            {
+                var neighbour = new Node(currentNode.Position + neighbourOffsets[i]);
+
+                if (!IsValidPosition(neighbour.Position))
+                    continue;
+                if (closedList.Contains(neighbour) || openList.Contains(neighbour))
+                    continue;
+
+                neighbour.parent = currentNode;
+                neighbour.H = math.abs(neighbour.Position.x - endNode.Position.x) + math.abs(neighbour.Position.y - endNode.Position.y);
+                neighbour.G = currentNode.G + 1;
+
+                openList.Add(neighbour);
+            }
+        }
+
+        if (currentNode.Equals(endNode)) // Found goal
+        {
+            Node current = currentNode;
+
+            while (current != null)
+            {
+                path.Add(current.Position);
+                current = current.parent;
+            }
+        }
+
+        return path;
+    }
+
+    bool IsValidPosition(int2 pos)
+    {
+        if (pos.x > gridSize.x || pos.y > gridSize.y || pos.x < 0 || pos.y < 0) // outside of map
+            return false;
+
+        if (grid[GetIndex(pos.x, pos.y)] != 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    int GetIndex(int x, int y)
+    {
+        return x * gridSize.x + y;
+    }
+}
--- Assets/Scripts/ECS/Systems/Pathfinding/PathfindingSystem.cs
+++ Assets/Scripts/ECS/Systems/Pathfinding/PathfindingSystem.cs
@@ -16,14 +16,14 @@ public class PathfindingSystem : JobComponentSystem
     protected override void OnCreate()
     {
         neighbours = new NativeArray<int2>(8, Allocator.Persistent);
-        neighbours[0] = new int2(-1, -1);// Bottom left
         neighbours[1] = new int2(0, -1); // Bottom
-        neighbours[2] = new int2(1, -1); // Bottom Right
         neighbours[3] = new int2(-1, 0); // Left
         neighbours[4] = new int2(1, 0); // Right
-        neighbours[5] = new int2(-1, 1); // Top Left
         neighbours[6] = new int2(0, 1); // Top
-        neighbours[7] = new int2(1, 1); // Top Right
+        //neighbours[0] = new int2(-1, -1);// Bottom left
+        //neighbours[2] = new int2(1, -1); // Bottom Right
+        //neighbours[7] = new int2(1, 1); // Top Right
+        //neighbours[5] = new int2(-1, 1); // Top Left
     }
 
     protected override JobHandle OnUpdate(JobHandle inputDeps)
@@ -38,7 +38,6 @@ public class PathfindingSystem : JobComponentSystem
             NeighbourOffsets = neighbours,
             Path = path
         };
-
         JobHandle handle = job.Schedule();
         handle.Complete();
         path.Dispose();
@@ -70,97 +69,8 @@ public class PathfindingSystem : JobComponentSystem
 
         void FindPath(int2 start, int2 end)
         {
-            var openList = new NativeList<Node>(Allocator.Temp);
-            var closedList = new NativeList<Node>(Allocator.Temp);
-            if (start.Equals(end))
-                return;
-
-            var startNode = new Node(start);
-            openList.Add(startNode);
-
-            Node currentNode = new Node();
-
-            while (openList.Length > 0)
-            {
-                currentNode = openList[0];
-                openList.RemoveAtSwapBack(0);
-
-                closedList.Add(currentNode);
-
-                for (int i = 0; i < NeighbourOffsets.Length; i++)
-                {
-                    var neighbour = new Node(currentNode.Position + NeighbourOffsets[i]);
-                    if (Grid[GetIndex(neighbour.Position.x, neighbour.Position.y)] != 0)
-                        return;
-
-                    if (closedList.Contains(neighbour) || openList.Contains(neighbour))
-                        continue;
-
-                    neighbour.ParentIndex = openList.IndexOf(currentNode);
-                    neighbour.H = math.abs(neighbour.Position.x - end.x) + math.abs(neighbour.Position.y - end.y);
-                    neighbour.G = currentNode.G + 1;
-
-                    openList.Add(neighbour);
-                }
-            }
-
-            if (currentNode.Equals(new Node(end)))
-            {
-                Node current = currentNode;
-                while (current.ParentIndex != -1)
-                {
-                    Path.Add(current.Position);
-                    current = openList[current.ParentIndex];
-                }
-            }
-
-            openList.Dispose();
-            closedList.Dispose();
-
-        }
-
-        int GetIndex(int x, int y)
-        {
-            return x * gridSize.x + y;
-        }
-
-        struct Node : IEquatable<Node>
-        {
-            public int G;
-            public int H;
-            public int F
-            {
-                get
-                {
-                    return H + G;
-                }
-            }
-
-            public int2 Position;
-            public int ParentIndex;
-
-            public Node(int2 position)
-            {
-                Position = position;
-                G = 0;
-                H = 0;
-
-                ParentIndex = -1;
-            }
-
-            public int CompareTo(Node other)
-            {
-                int compare = F.CompareTo(other.F);
-                if (compare == 0)
-                    compare = H.CompareTo(other.H);
-
-                return -compare;
-            }
-
-            public bool Equals(Node other)
-            {
-                return G == other.G && H == other.H && Position.Equals(other.Position);
-            }
+            AStar aStar = new AStar();
+            Path = aStar.GetPath(Grid, gridSize, NeighbourOffsets, start, end);
         }
     }
 }
