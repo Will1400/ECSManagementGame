4f15225ec40b47023ae733a59157ae3ea3130b04
Will1400 <Will1400@edu.campusvejle.dk>
2020-03-17 09:06:47 +0100

    Fixed commandbuffer not being played back

--- Assets/Scripts/ECS/Systems/Movement/NavAgentMovementSystem.cs
+++ Assets/Scripts/ECS/Systems/Movement/NavAgentMovementSystem.cs
@@ -8,33 +8,32 @@ using Unity.Collections;
 
 public class NavAgentMovementSystem : JobComponentSystem
 {
-    EntityCommandBuffer commandBuffer;
+
+    EndSimulationEntityCommandBufferSystem bufferSystem;
+
 
     protected override void OnCreate()
     {
-        commandBuffer = new EntityCommandBuffer(Allocator.Persistent);
-
+        bufferSystem = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
     }
+
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
         var job = new MoveJob
         {
             DeltaTime = Time.DeltaTime,
-            CommandBuffer = commandBuffer.ToConcurrent()
+            CommandBuffer = bufferSystem.CreateCommandBuffer().ToConcurrent()
         }.Schedule(this, inputDeps);
 
         job.Complete();
-
         return job;
     }
 
-    protected override void OnDestroy()
-    {
-        commandBuffer.Dispose();
-    }
+    [RequireComponentTag(typeof(AgentHasPath))]
     struct MoveJob : IJobForEachWithEntity_EBCCC<Float3BufferElement, Translation, MoveSpeed, NavAgent>
     {
         public float DeltaTime;
+
         public EntityCommandBuffer.Concurrent CommandBuffer;
 
         public void Execute(Entity entity, int index, DynamicBuffer<Float3BufferElement> buffer, ref Translation translation, [ReadOnly]ref MoveSpeed speed, ref NavAgent agent)
@@ -42,7 +41,7 @@ public class NavAgentMovementSystem : JobComponentSystem
             if (agent.CurrentWaypointIndex >= buffer.Length)
             {
                 agent.Status = AgentStatus.Idle;
-                CommandBuffer.RemoveComponent<AgentHasPath>(index, entity);
+                CommandBuffer.RemoveComponent(index, entity, typeof(AgentHasPath));
                 return;
             }
             float3 destination = buffer[agent.CurrentWaypointIndex];
