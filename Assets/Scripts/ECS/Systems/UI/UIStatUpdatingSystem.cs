c7fa46546967df26e29d3c30e3f443f7b3b6a1e8
Will1400 <Will1400@edu.campusvejle.dk>
2020-03-23 12:17:12 +0100

    Added citizen counter

---
+++ Assets/Scripts/ECS/Systems/UI/UIStatUpdatingSystem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.Entities;
+using UnityEngine.Events;
+using System;
+
+public class UIStatUpdatingSystem : SystemBase
+{
+    public static UIStatUpdatingSystem Instance;
+
+    public Action<int> CitizenCountChanged;
+
+    int citizenCount;
+    EntityQuery citizens;
+
+    protected override void OnCreate()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        citizens = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new ComponentType[] { typeof(Citizen) }
+        });
+    }
+
+    protected override void OnUpdate()
+    {
+        int newCount = citizens.CalculateEntityCount();
+
+        if (citizenCount != newCount)
+        {
+            CitizenCountChanged?.Invoke(newCount);
+            citizenCount = newCount;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/CitizenUICounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Entities;
+using UnityEngine;
+
+public class CitizenUICounter : MonoBehaviour
+{
+    public TextMeshProUGUI CountText;
+
+    private void Start()
+    {
+        SetCount(0);
+    }
+
+    private void OnEnable()
+    {
+        UIStatUpdatingSystem.Instance.CitizenCountChanged += SetCount;
+    }
+
+    public void SetCount(int count)
+    {
+        CountText.text = count.ToString();
+    }
+
+    private void OnDisable()
+    {
+        UIStatUpdatingSystem.Instance.CitizenCountChanged -= SetCount;
+
+    }
+}
