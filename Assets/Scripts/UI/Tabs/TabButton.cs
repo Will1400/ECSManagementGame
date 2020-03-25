ef4495364e75437dd83491aeba3a8b8892f7b888
Will1400 <Will1400@edu.campusvejle.dk>
2020-03-25 11:33:41 +0100

    Added ui tab group

---
+++ Assets/Scripts/UI/Tabs/TabButton.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public UnityEvent OnTabSelected;
+    public UnityEvent OnTabDeselected;
+
+
+    [SerializeField]
+    private TabGroup tabGroup;
+
+    [SerializeField]
+    private Image background;
+    [SerializeField]
+    private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        if (OnTabSelected == null)
+            OnTabSelected = new UnityEvent();
+
+        if (OnTabDeselected == null)
+            OnTabDeselected = new UnityEvent();
+    }
+
+    void Start()
+    {
+        tabGroup.Subscribe(this);
+    }
+
+    public void SetColor(Color backgroundColor, Color textColor)
+    {
+        background.color = backgroundColor;
+        text.color = textColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+
+    public void Select()
+    {
+        OnTabSelected?.Invoke();
+    }
+
+    public void Deselect()
+    {
+        OnTabDeselected?.Invoke();
+    }
+}
---
+++ Assets/Scripts/UI/Tabs/TabGroup.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabGroup : MonoBehaviour
+{
+    [SerializeField]
+    private List<TabButton> tabButtons;
+
+    [SerializeField]
+    private Color tabIdle;
+    [SerializeField]
+    private Color tabTextIdle;
+    [SerializeField]
+    private Color tabHover;
+    [SerializeField]
+    private Color tabTextHover;
+    [SerializeField]
+    private Color tabSelected;
+    [SerializeField]
+    private Color tabTextSelected;
+
+    TabButton selectedTab;
+
+    private void Awake()
+    {
+        tabButtons = new List<TabButton>();
+    }
+
+    public void Subscribe(TabButton tabButton)
+    {
+        tabButtons.Add(tabButton);
+        tabButton.SetColor(tabIdle, tabTextIdle);
+    }
+
+    public void OnTabEnter(TabButton tabButton)
+    {
+        ResetTabs();
+
+        if (tabButton != selectedTab)
+            tabButton.SetColor(tabHover, tabTextHover);
+    }
+
+    public void OnTabExit(TabButton tabButton)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButton tabButton)
+    {
+        if (selectedTab != null)
+            selectedTab.Deselect();
+
+        selectedTab = tabButton;
+        selectedTab.Select();
+
+        ResetTabs();
+        tabButton.SetColor(tabSelected, tabTextSelected);
+    }
+
+
+    public void ResetTabs()
+    {
+        foreach (TabButton tabButton in tabButtons)
+        {
+            if (tabButton != selectedTab)
+                tabButton.SetColor(tabIdle, tabTextIdle);
+        }
+    }
+}
