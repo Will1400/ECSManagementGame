b615fd2227c755ee73c3c93e2e3bdead4a4a0e72
Will1400 <Will1400@edu.campusvejle.dk>
2020-03-31 11:06:35 +0200

    Converted to new api

--- Assets/Scripts/ECS/Systems/Construction/ConstructionSystem.cs
+++ Assets/Scripts/ECS/Systems/Construction/ConstructionSystem.cs
@@ -7,7 +7,7 @@ using Unity.Rendering;
 using Unity.Transforms;
 using UnityEngine;
 
-public class ConstructionSystem : JobComponentSystem
+public class ConstructionSystem : SystemBase
 {
     EndSimulationEntityCommandBufferSystem bufferSystem;
 
@@ -16,35 +16,22 @@ public class ConstructionSystem : JobComponentSystem
         bufferSystem = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
     }
 
-    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    protected override void OnUpdate()
     {
-        var job = new ConstructionJob
-        {
-            DeltaTime = Time.DeltaTime,
-            CommandBuffer = bufferSystem.CreateCommandBuffer().ToConcurrent()
-            
-        }.Schedule(this, inputDeps);
+        EntityCommandBuffer.Concurrent CommandBuffer = bufferSystem.CreateCommandBuffer().ToConcurrent();
+        float deltaTime = Time.DeltaTime;
 
-        job.Complete();
-        return job;
-    }
-
-    struct ConstructionJob : IJobForEachWithEntity<UnderConstruction, WorkPlaceWorkerData>
-    {
-        public EntityCommandBuffer.Concurrent CommandBuffer;
-        public float DeltaTime;
-
-        public void Execute(Entity entity, int index, ref UnderConstruction construction, ref WorkPlaceWorkerData workerData)
+        Entities.ForEach((Entity entity, int entityInQueryIndex, ref UnderConstruction construction, ref WorkPlaceWorkerData workerData) =>
         {
             if (workerData.ActiveWorkers >= 0)
             {
-                construction.remainingConstructionTime -= DeltaTime * (workerData.ActiveWorkers / .5f);
+                construction.remainingConstructionTime -= deltaTime * (workerData.ActiveWorkers / .5f);
             }
 
             if (construction.remainingConstructionTime <= 0)
             {
-                CommandBuffer.AddComponent<ConstructionFinishedTag>(index, entity);
+                CommandBuffer.AddComponent<ConstructionFinishedTag>(entityInQueryIndex, entity);
             }
-        }
+        }).Schedule();
     }
 }
