8ea26a3a57846c92f5ea2e7340441cbbc680f4cb
Will1400 <Will1400@edu.campusvejle.dk>
2020-03-31 11:06:48 +0200

    Added resource request base

---
+++ Assets/Scripts/ECS/Components/Resources/ResourceRequest.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.Entities;
+
+public struct ResourceRequest : IComponentData
+{
+    public Entity RequestingEntity;
+    public ResourceType ResourceType;
+    public int Amount;
+}
