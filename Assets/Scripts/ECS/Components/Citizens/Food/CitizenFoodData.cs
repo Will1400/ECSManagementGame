aba1b9fe4419013074e50cde984adfed87b39743
Will1400 <Will1400@edu.campusvejle.dk>
2020-04-28 11:31:59 +0200

    Added citizen food component

---
+++ Assets/Scripts/ECS/Components/Citizens/Food/CitizenFoodData.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct CitizenFoodData : IComponentData
+{
+    public float MaxFoodLevel;
+    public float CurrentFoodLevel;
+    public float DepletionMultiplier;
+}
