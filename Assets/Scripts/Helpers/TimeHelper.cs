f593623226225031d4fc9b5d4b338bccebac3c06
Will1400 <Will1400@edu.campusvejle.dk>
2020-04-28 11:23:08 +0200

    Added time helper

---
+++ Assets/Scripts/Helpers/TimeHelper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.Burst;
+
+[BurstCompile]
+public static class TimeHelper
+{
+    public static readonly float RealTimeSecondRatio = 60;
+
+    [BurstCompile]
+    public static float ConvertTime(int hours, int minutes, float seconds)
+    {
+        minutes += hours * 60;
+        seconds += minutes * 60;
+
+        return seconds * RealTimeSecondRatio;
+    }
+
+    public static float ConvertTime(int minutes, float seconds)
+    {
+        return ConvertTime(0, minutes, seconds);
+    }
+
+    public static float ConvertTime(float seconds)
+    {
+        return ConvertTime(0, 0, seconds);
+    }
+}
