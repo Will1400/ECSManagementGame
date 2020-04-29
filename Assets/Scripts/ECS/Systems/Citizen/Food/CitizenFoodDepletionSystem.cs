55fa29be4a1fc280327b04f6cbe1c5e5bcb85988
Will1400 <Will1400@edu.campusvejle.dk>
2020-04-29 09:21:14 +0200

    Added food deletion system

---
+++ Assets/Scripts/ECS/Systems/Citizen/Food/CitizenFoodDepletionSystem.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class CitizenFoodDepletionSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float deltatime = Time.DeltaTime;
+
+        Entities.ForEach((Entity entity, ref CitizenFoodData foodData) =>
+        {
+            foodData.CurrentFoodLevel -= deltatime / 4 * foodData.DepletionMultiplier;
+        }).Schedule();
+    }
+}
