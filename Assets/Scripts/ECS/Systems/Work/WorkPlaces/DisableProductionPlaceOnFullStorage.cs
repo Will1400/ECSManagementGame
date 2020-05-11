0c7d93a3e42ae0db0a6c31e8ccb99a5cf2f1eb30
Will1400 <Will1400@edu.campusvejle.dk>
2020-05-11 10:32:12 +0200

    Workers get removed from production workplaces when storage is full

---
+++ Assets/Scripts/ECS/Systems/Work/WorkPlaces/DisableProductionPlaceOnFullStorage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.Entities;
+using Unity.Collections;
+
+[UpdateBefore(typeof(FireAllWorkersFromWorkplaceSystem))]
+public class DisableProductionPlaceOnFullStorage : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var CommandBuffer = new EntityCommandBuffer(Allocator.TempJob);
+
+        Entities.ForEach((Entity entity, ref ResourceProductionData productionData, ref ResourceStorageData resourceStorage, ref WorkplaceWorkerData workerData) =>
+        {
+            if (resourceStorage.UsedCapacity >= resourceStorage.MaxCapacity)
+            {
+                workerData.IsWorkable = false;
+                if (workerData.ActiveWorkers > 0)
+                    CommandBuffer.AddComponent<FireAllWorkersTag>(entity);
+            }
+            else
+            {
+                workerData.IsWorkable = true;
+            }
+
+        }).Schedule(Dependency).Complete();
+
+        CommandBuffer.Playback(EntityManager);
+        CommandBuffer.Dispose();
+    }
+}
---
+++ Assets/Scripts/ECS/Systems/Work/WorkPlaces/FireAllWorkersFromWorkplaceSystem.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Burst;
+
+public class FireAllWorkersFromWorkplaceSystem : SystemBase
+{
+    EntityQuery workplacesToFireQuery;
+    EntityQuery workingCitizensQuery;
+    EndSimulationEntityCommandBufferSystem bufferSystem;
+
+    protected override void OnCreate()
+    {
+        bufferSystem = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+
+        workplacesToFireQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new ComponentType[] { typeof(FireAllWorkersTag), typeof(WorkplaceWorkerData) }
+        });
+        workingCitizensQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new ComponentType[] { typeof(CitizenWork) },
+            None = new ComponentType[] { typeof(RemoveFromWorkTag) }
+        });
+    }
+
+    protected override void OnUpdate()
+    {
+        if (workplacesToFireQuery.CalculateChunkCount() <= 0)
+            return;
+
+        NativeArray<Entity> workingCitizens = workingCitizensQuery.ToEntityArray(Allocator.TempJob);
+        NativeArray<CitizenWork> workingCitizensWorkerData = workingCitizensQuery.ToComponentDataArray<CitizenWork>(Allocator.TempJob);
+
+        var job = new RemoveWorkplaceJob
+        {
+            EntityType = GetArchetypeChunkEntityType(),
+            WorkplaceWorkerDataType = GetArchetypeChunkComponentType<WorkplaceWorkerData>(),
+            WorkingCitizens = workingCitizens,
+            WorkingCitizensWorkerData = workingCitizensWorkerData,
+            CommandBuffer = bufferSystem.CreateCommandBuffer().ToConcurrent()
+        }.Schedule(workplacesToFireQuery);
+        job.Complete();
+
+        workingCitizens.Dispose();
+        workingCitizensWorkerData.Dispose();
+    }
+
+    [BurstCompile]
+    struct RemoveWorkplaceJob : IJobChunk
+    {
+        [ReadOnly]
+        public ArchetypeChunkEntityType EntityType;
+        public ArchetypeChunkComponentType<WorkplaceWorkerData> WorkplaceWorkerDataType;
+
+        [ReadOnly]
+        public NativeArray<Entity> WorkingCitizens;
+        [ReadOnly]
+        public NativeArray<CitizenWork> WorkingCitizensWorkerData;
+
+        public EntityCommandBuffer.Concurrent CommandBuffer;
+
+        public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
+        {
+            var entities = chunk.GetNativeArray(EntityType);
+            var workplaceWorkerDatas = chunk.GetNativeArray(WorkplaceWorkerDataType);
+
+            for (int i = 0; i < chunk.Count; i++)
+            {
+                WorkplaceWorkerData workplaceWorkerData = workplaceWorkerDatas[i];
+
+                for (int j = 0; j < WorkingCitizensWorkerData.Length; j++)
+                {
+                    if (workplaceWorkerData.CurrentWorkers <= 0)
+                        break;
+
+                    var workerData = WorkingCitizensWorkerData[j];
+                    if (workerData.WorkplaceEntity.Index == entities[i].Index)
+                    {
+                        CommandBuffer.AddComponent<RemoveFromWorkTag>(chunkIndex, WorkingCitizens[j]);
+                        workplaceWorkerData.CurrentWorkers--;
+                    }
+                }
+
+                if (workplaceWorkerData.CurrentWorkers <= 0)
+                    CommandBuffer.RemoveComponent<FireAllWorkersTag>(chunkIndex, entities[i]);
+            }
+        }
+    }
+}
